81de54e54f01d7db7f4363df91d84cf39a176764
lifeng <lifeng@ipfsforce.com>
2020-07-10 17:20:00 +0800

    updates

--- cmd/lotus-seal-worker/main.go
+++ cmd/lotus-seal-worker/main.go
@@ -168,7 +168,7 @@ var runCmd = &cli.Command{
 
 		var taskTypes []sealtasks.TaskType
 
-		taskTypes = append(taskTypes, sealtasks.TTCommit1, sealtasks.TTFinalize)
+		taskTypes = append(taskTypes, sealtasks.TTFetch, sealtasks.TTCommit1, sealtasks.TTFinalize)
 
 		if cctx.Bool("precommit1") {
 			taskTypes = append(taskTypes, sealtasks.TTPreCommit1)
